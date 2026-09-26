90a91faa450a92542ee6619017be9293e52406c9
Samia39 <samiairshad090@gmail.com>
2026-09-26 21:38:21 +0500

    Change in order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -178,13 +178,15 @@ class OrderController extends Controller
                 'status' => $paymentStatus,
             ]);
 
-            // Notify admins a payment needs approval
-            NotificationService::sendToAdmins(
-                'payment_pending',
-                'New payment submitted',
-                'Order ' . $order->order_number . ' has a payment awaiting approval.',
-                ['order_id' => $order->id]
-            );
+            // Notify admins a payment needs approval skip for card
+            if ($request->payment_method !== 'card') {
+                NotificationService::sendToAdmins(
+                    'payment_pending',
+                    'New payment submitted',
+                    'Order ' . $order->order_number . ' has a payment awaiting approval.',
+                    ['order_id' => $order->id]
+                );
+            }   
 
 
             DB::commit();
